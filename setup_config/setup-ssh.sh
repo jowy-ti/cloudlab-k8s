a4bc22bd719fb3acac317f46d165f0d7eb0a9f07
joel.gonzalez.jimenez <joel.gonzalez.jimenez@estudiantat.upc.edu>
2025-09-23 18:43:46 +0000

    ssh script a probar

--- setup_config/setup-ssh.sh
+++ setup_config/setup-ssh.sh
@@ -8,7 +8,8 @@ FIRST_WORKER="node2"
 
 if [[ $NODENUM == "node1" ]]; then
     run_command  "ssh-keygen -t ed25519 -f $SSH_DIR/$KEY -q -N """ "Creando par de claves para ssh"
-    cp "$SSH_DIR/$KEY.pub" "$SHARED_DIR/$KEY.pub"
+    run_command "cp $SSH_DIR/$KEY.pub $SHARED_DIR/$KEY.pub" "Copiando la clave pública al directorio compartido"
+    run_command "cat $SSH_DIR/$KEY.pub >> $SSH_DIR/authorized_keys" "Añadiendo la clave pública en authorized keys"
 
     # Comprobación del agente ssh
     if ssh-add -l > /dev/null 2>&1; then
@@ -37,10 +38,16 @@ else
 
     if [[ $NODENUM != $FIRST_WORKER ]]; then
 
-        NUM= $((${NODENUM:4} - 1))  
+        NUM=$((${NODENUM:4} - 1))  
         NODE_WAIT="node${NUM}"
 
         while [[ ! -f "$SHARED_DIR/$NODE_WAIT" ]]; do
             sleep $WAIT_TIME
         done
     fi
+
+    run_command "cp $SHARED_DIR/$KEY.pub $SSH_DIR/$KEY.pub" "Copiando la clave pública al directorio personal ~/.ssh"
+    run_command "cat $SSH_DIR/$KEY.pub >> $SSH_DIR/authorized_keys" "Añadiendo la clave pública en authorized keys"
+    run_command "touch $SHARED_DIR/$NODENUM" "Creando el fichero para avisar configuración ssh finalizada"
+
+fi
--- setup_config/setup.sh
+++ setup_config/setup.sh
@@ -36,7 +36,8 @@ fi
 export readonly NUM_WORKERS=$2
 
 # Pasos para establecer las claves en los nodos
-$SETUP_DIR/setup-ssh.sh
+# $SETUP_DIR/setup-ssh.sh
+# exit 0
 
 # Si el nodo no es el master se detiene la configuración
 if [[ $NODENUM != "node1" ]]; then
